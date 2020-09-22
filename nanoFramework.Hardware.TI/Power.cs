a51c9efa63222501dc5a5915a43c3d611009dfe6
josesimoes <jose.simoes@eclo.solutions>
2020-09-22 18:56:42 +0100

    Add EnterStandbyMode API
    - Fix some typos in the docs along the way.

--- nanoFramework.Hardware.TI/Power.cs
+++ nanoFramework.Hardware.TI/Power.cs
@@ -17,12 +17,12 @@ namespace nanoFramework.Hardware.TI
     public static partial class Power
     {
         /// <summary>
-        /// Sets the target device to enter TI "shudown" mode.
+        /// Sets the target device to enter TI "shutdown" mode.
         /// </summary>
         /// <remarks>
         /// If no wakeup sources configured then it will be a indefinite sleep.
         /// This call never returns.
-        /// After the device enters standby a wakeup source will wake the device and the execution will start as if it was a reset.
+        /// After the device enters shutdown a wakeup source will wake the device and the execution will start as if it was a reset.
         /// Keep in mind that the execution WILL NOT continue after the call to this method.
         /// </remarks>
         public static void EnterShutdownMode()
@@ -34,12 +34,28 @@ namespace nanoFramework.Hardware.TI
         }
 
         /// <summary>
+        /// Sets the target device to enter TI "standby" mode.
+        /// </summary>
+        /// <param name="duration">Duration of the standby mode.</param>
+        /// <remarks>
+        /// After the device enters standby it will wake-up after the set period of time .
+        /// Keep in mind that the execution WILL NOT continue after the call to this method.
+        /// </remarks>
+        public static void EnterStandbyMode(TimeSpan duration)
+        {
+            NativeEnterStandbyMode(duration);
+
+            // force an infinite sleep to allow execution engine to exit this thread and pick the reboot flags set on the call above
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+        }
+
+        /// <summary>
         /// Setup wakeup configuration from GPIO pins.
         /// </summary>
         /// <param name="pinWakeupConfig">Array of <see cref="PinWakeupConfig"/> with the configuration for the GPIO pins.</param>
         /// <remarks>
         /// This method is used to setup the wakeup configuration from GPIO pins.
-        /// If there is a need to make furhter changes to the configuration just call this method again with the new configuration.
+        /// If there is a need to make further changes to the configuration just call this method again with the new configuration.
         /// There is no storage for the configuration nor any method to query the existing configuration, if any.
         /// If a GPIO pin has been setup with a wakeup configuration and that needs to be removed, it should be added to the new configuration
         /// array with <see cref="PinWakeupEdge.None"/>.
@@ -80,11 +96,11 @@ namespace nanoFramework.Hardware.TI
         //[MethodImpl(MethodImplOptions.InternalCall)]
         //public static extern void EnableWakeupPin(WakeupPin pin = WakeupPin.Pin1);
 
-        /// <summary>
-        /// Sets the target device to enter TI "shutdown" mode.
-        /// </summary>
         [MethodImpl(MethodImplOptions.InternalCall)]
         private static extern void NativeEnterShutdownMode();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void NativeEnterStandbyMode(TimeSpan duration);
 
         #endregion
 
